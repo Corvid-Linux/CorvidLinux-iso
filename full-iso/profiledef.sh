0a793447eeb9fb88b87c492439fa08c771f279c1
Wan Ashraf <73482535+Ashraf-wan@users.noreply.github.com>
2021-11-28 17:34:56 +0800

    Update profiledef.sh

--- full-iso/profiledef.sh
+++ full-iso/profiledef.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2034
-
-iso_name="corvid-linux-full"
-iso_label="CORVIDLINUX_$(date +%Y%m)"
-iso_publisher="Corvid Linux <https://www.blackarch.org/>"
-iso_application="Corvid Linux Full ISO"
-iso_version="$(date +%Y.%m.%d)"
-install_dir="corvid"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
-arch="x86_64"
-pacman_conf="pacman.conf"
+...
+file_permissions=(
+  ...
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
+)
