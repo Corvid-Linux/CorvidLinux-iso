2f60d06e9c72c15b1c567c7c06666d32b7b96ccb
Wan Ashraf <73482535+Ashraf-wan@users.noreply.github.com>
2021-11-28 17:23:21 +0800

    Update profiledef.sh

--- full-iso/profiledef.sh
+++ full-iso/profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="blackarch-linux-full"
-iso_label="BLACKARCH_$(date +%Y%m)"
-iso_publisher="BlackArch Linux <https://www.blackarch.org/>"
-iso_application="BlackArch Linux Full ISO"
+iso_name='corvid-linux-full"
+iso_label="CORVIDLINUX_$(date +%Y%m)"
+iso_publisher="Corvid Linux <https://www.blackarch.org/>"
+iso_application="Corvid Linux Full ISO"
 iso_version="$(date +%Y.%m.%d)"
-install_dir="blackarch"
+install_dir="corvid"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
